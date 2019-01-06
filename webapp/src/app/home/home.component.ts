eb6f6d96a95f3791c4e070e6299d36c1e569847e
Marton Cserei <warcodhiro@gmail.com>
2019-01-06 18:31:14 +0200

    add search input and reformat code

--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  keyword: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
