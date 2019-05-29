3216da21c846b6ffde640947936174e7420ab823
Marton Cserei <warcodhiro@gmail.com>
2019-05-29 21:43:58 +0300

    add image

--- webapp/src/app/chocolate/chocolate-details/chocolate-details.component.ts
+++ webapp/src/app/chocolate/chocolate-details/chocolate-details.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chocolate-details.component.scss']
 })
 export class ChocolateDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
