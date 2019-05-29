dcf1954be0c964ee4ffbd4af2684425aae4370d8
Marton Cserei <warcodhiro@gmail.com>
2019-05-29 22:06:57 +0300

    add stars section

--- webapp/src/app/chocolate/chocolate-details/chocolate-details.component.ts
+++ webapp/src/app/chocolate/chocolate-details/chocolate-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChocolatePreview } from '../models';
 
 @Component({
   selector: 'choki-chocolate-details',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chocolate-details.component.scss']
 })
 export class ChocolateDetailsComponent implements OnInit {
+  chocolate: ChocolatePreview = {
+    description: 'Great taste',
+    image: 'milka',
+    brand: 'Milka',
+    rating: 2,
+    type: 'Hazelnut'
+  };
+
   constructor() {}
 
   ngOnInit() {}
+
+  get fullStars() {
+    if (!this.chocolate) {
+      return [];
+    }
+    return new Array(3);
+  }
+
+  get hollowStars() {
+    if (!this.chocolate) {
+      return [];
+    }
+    return new Array(5 - 3);
+  }
 }
--- webapp/src/app/chocolate/models/chocolate.model.ts
+++ webapp/src/app/chocolate/models/chocolate.model.ts
@@ -5,3 +5,17 @@ export class ChocolatePreview {
   brand: string;
   type: string;
 }
+
+export class Chocolate {
+  rating: number;
+  image: string;
+  description: string;
+  brand: string;
+  type: string;
+  author: User;
+}
+
+export class User {
+  name: string;
+  image: string;
+}
