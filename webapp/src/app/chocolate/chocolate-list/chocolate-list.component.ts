cb09a2b5fb3c6a0f1ce1f25ce43eeeffe2e45836
Marton Cserei <warcodhiro@gmail.com>
2019-05-29 19:00:53 +0300

    test commit

--- webapp/src/app/chocolate/chocolate-list/chocolate-list.component.ts
+++ webapp/src/app/chocolate/chocolate-list/chocolate-list.component.ts
@@ -11,7 +11,7 @@ export class ChocolateListComponent implements OnInit {
   keyword: string;
   chocolates: ChocolatePreview[] = [
     { description: "The finest one out there", image: 'milka', brand: "Milka", rating: 4, type: 'Milk' },
-    { description: "Great taste ", image: 'milka', brand: "Milka", rating: 2, type: 'Hazelnut' }
+    { description: "Great taste test", image: 'milka', brand: "Milka", rating: 2, type: 'Hazelnut' }
   ];
 
   constructor(private activatedRoute: ActivatedRoute) { }
