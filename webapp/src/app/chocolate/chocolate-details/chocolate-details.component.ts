6e7e771ddf93df095f6a25943d998fc48637cd82
Marton Cserei <warcodhiro@gmail.com>
2019-05-29 19:33:39 +0300

    add chocolate details component

---
+++ webapp/src/app/chocolate/chocolate-details/chocolate-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'choki-chocolate-details',
+  templateUrl: './chocolate-details.component.html',
+  styleUrls: ['./chocolate-details.component.scss']
+})
+export class ChocolateDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
